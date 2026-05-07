7d996387d1f460437bb3ea98e7b3c4ff5b26c754
Randy Heiland <randy.heiland@gmail.com>
2026-05-07 16:06:16 -0400

    update

--- beta/plot_cell_scalars_4states.py
+++ beta/plot_cell_scalars_4states.py
@@ -449,9 +449,14 @@ class Vis():
                     # light blue: 171,217,233
                     # blue: 44,123,182
 
-                    # cbar_name = from_list(None, [[44./255, 123./255, 182./255],[171./255,217./255,233./255],[253./255,174./255,97./255],[215./255,25./255,28./255]], len(self.discrete_variable))
-                    cbar_name = from_list(None, [[44/255.,123/255.,182/255.], [253/255.,174/255.,97/255.], 
-                        [171/255.,217/255.,233/255.],[215/255.,25/255.,28/255.]], len(self.discrete_variable))
+                    # Roman's: c=0 dark blue (no contact inhibition), c=1 light blue (pressure CI), c=2 orange (surface CI), c=3 red (doubly inhibited)
+                    vmin = 0
+                    vmax = 3
+                    cbar_name = from_list(None, [[44./255, 123./255, 182./255], [171./255,217./255,233./255], [253./255,174./255,97./255], [215./255,25./255,28./255]], len(self.discrete_variable))
+                    print(f"cbar_name={cbar_name}, len(self.discrete_variable)={len(self.discrete_variable)}")
+                    # Randy,rwh experiment
+                    # cbar_name = from_list(None, [[44/255.,123/255.,182/255.], [253/255.,174/255.,97/255.], 
+                        # [171/255.,217/255.,233/255.],[215/255.,25/255.,28/255.]], len(self.discrete_variable))
                     # print("cmaps.paint_clist=",cmaps.paint_clist)
                     # print("cbar_name=",cbar_name) # <matplotlib.colors.LinearSegmentedColormap
                 except:
@@ -459,8 +464,8 @@ class Vis():
 
             # usual categorical colormap on matplotlib has at max 20 colors (using colorcet the colormap glasbey_bw has n colors )
             # cbar_name = from_list(None, cc.glasbey_bw, len(self.discrete_variable))
-            vmin = None
-            vmax = None
+            # vmin = None
+            # vmax = None
             # Change the values between 0 and number of possible values
             for i, value in enumerate(self.discrete_variable):
                 cell_scalar = cell_scalar.replace(value,i)
@@ -545,6 +550,7 @@ class Vis():
                 try:
                     # print("plot circles with vmin,vmax=",vmin,vmax)  # None,None
                     # print("plot circles with cbar_name=",cbar_name)  # <matplotlib.colors.LinearSegmentedColormap object at 0x1690d5330>
+                    print("------- calling circles #1 with vmin,vmax=",vmin,vmax)
                     cell_plot = self.circles(xvals,yvals, s=cell_radii, c=cell_scalar, edgecolor='black', linewidth=self.cell_line_width, cmap=cbar_name, vmin=vmin, vmax=vmax)
                     # cell_plot = self.circles(xvals,yvals, s=cell_radii, edgecolor=cell_scalar, linewidth=0.5, cmap=cbar_name, vmin=vmin, vmax=vmax)
                 except (ValueError):
@@ -552,9 +558,13 @@ class Vis():
                     pass
             else:
                 # cell_plot = self.circles(xvals,yvals, s=cell_radii, c=cell_scalar, cmap=cbar_name)
+                print("------- calling circles #2 with vmin,vmax=",vmin,vmax)   # rwh: doing this!
                 cell_plot = self.circles(xvals,yvals, s=cell_radii, c=cell_scalar, cmap=cbar_name, vmin=vmin, vmax=vmax)
+                # cell_plot = self.circles(xvals,yvals, s=cell_radii, c=cell_scalar, cmap=cbar_name, vmin=0, vmax=3)
+                # cell_plot = self.circles(xvals,yvals, s=cell_radii, c=cell_scalar, cmap=cbar_name)
 
         else:  # semi-trransparent cell, but with (thicker) edge  (TODO: how to make totally transparent?)
+            print("------- calling circles #3 with vmin,vmax=",vmin,vmax)
             if (self.cell_edge):
                 cell_plot = self.circles(xvals,yvals, s=cell_radii, c=cell_scalar, edgecolor='black', linewidth=self.cell_line_width2, cmap=cbar_name, vmin=vmin, vmax=vmax, alpha=self.cell_alpha)
             else:
